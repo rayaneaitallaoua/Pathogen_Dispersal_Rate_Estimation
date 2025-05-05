cf4c11a65e85cf25a93883ad6d6140de5b020125
Rayane Ayoub AIT ALLAOUA <ayoubaitallaoua@gmail.com>
2025-05-05 15:41:17 +0200

    addition: generate_gspace_settings_circular_sample()

--- utils/gspace_utils.py
+++ utils/gspace_utils.py
@@ -1,6 +1,7 @@
 import random
 import os
 import subprocess
+import math
 
 
 def generate_gspace_settings(output_dir=".",
@@ -166,4 +167,96 @@ def generate_gspace_settings_square_sample(output_dir=".",
     with open(f'{output_dir}/GSpacesettings_r_{r}.txt', "w") as f:
         f.write(gspace_settings)
 
-    print(f"GSpaceSettings_r_{r}.txt generated with random sampling coordinates in {output_dir}!")
+    print(f"GSpaceSettings_r_{r}.txt generated with square sampling coordinates in {output_dir}!")
+
+
+
+def generate_gspace_settings_circular_sample(output_dir=".",
+                                             r=3,
+                                             lattice_size_x=20,
+                                             lattice_size_y=20,
+                                             mutation_rate=1E-5,
+                                             num_sampled_nodes=4,
+                                             ind_per_node_sampled=5):
+    # find lattice center
+    lattice_center = (lattice_size_x // 2, lattice_size_y // 2)
+    sampled_positions = set()
+
+    # find the neighbour node presenting the least distance
+    def closest_lattice_node(x, y):
+
+        # candidates are the 4 neighbouring nodes
+        candidates = [(math.floor(x), math.floor(y)),
+                      (math.floor(x), math.ceil(y)),
+                      (math.ceil(x), math.floor(y)),
+                      (math.ceil(x), math.ceil(y))]
+
+        # define a lambda  function that returns the distance between our point pt and the candidates,
+        # iterates over candidates
+        return min(candidates, key=lambda pt: math.dist((x, y), pt))
+
+    # generate random phi and r' values
+    while len(sampled_positions) < num_sampled_nodes:
+        phi = random.uniform(0, 2 * math.pi)
+        r_prime = random.uniform(0, r)
+
+        # convert Polar coordinates to Cartesian
+        x = lattice_center[0] + r_prime * math.cos(phi)
+        y = lattice_center[1] + r_prime * math.sin(phi)
+
+        node = closest_lattice_node(x, y)
+
+        # Ensure node is inside the lattice bounds
+        if 0 <= node[0] < lattice_size_x and 0 <= node[1] < lattice_size_y:
+            sampled_positions.add(node)
+
+    sample_x = ",".join(str(pos[0]) for pos in sampled_positions)
+    sample_y = ",".join(str(pos[1]) for pos in sampled_positions)
+
+    gspace_settings = f"""%%%%%%%% SIMULATION SETTINGS %%%%%%%%%%%%%%%
+    Data_filename=sim_seqs_r_{r}
+    Run_Number=1
+
+    %%%%%%%% OUTPUT FILE FORMAT SETTINGS %%%%%%%
+    Output_Dir=.
+    %Coordinate_file=true
+    %Sequence_characteristics_file=true
+    Fasta=true
+    Fasta_Single_Line_Seq=True
+
+    %%%%%%%% MARKERS SETTINGS %%%%%%%%%%%%%%%%%%
+    Ploidy=Haploid
+    Chromosome_number=1
+    Sequence_Size=1000
+    Mutation_Model=HKY
+    Mutation_Rate={mutation_rate}
+
+    %%%%%%%% RECOMBINATION SETTINGS %%%%%%%%%%%%
+    Recombination_Rate=0
+
+    %%%%%%%% DEMOGRAPHIC SETTINGS %%%%%%%%%%%%%%
+    %% LATTICE
+    Lattice_Size_X={lattice_size_x}
+    Lattice_Size_Y={lattice_size_y}
+    Ind_Per_Pop=30
+
+    %% DISPERSAL
+    Dispersal_Distribution=uniform
+    Disp_Dist_Max=1,1
+    Total_Emigration_Rate=0.05
+
+    %%%%%%%% SAMPLE SETTINGS %%%%%%%%%%%%%%%%%%%
+    Sample_Size_X={len(sample_x.split(","))}
+    Sample_Size_Y={len(sample_y.split(","))}
+
+    %% Lattice center: {lattice_center} %%
+    SampleCoordinateX={sample_x}
+    SampleCoordinateY={sample_y}
+    Ind_Per_Node_Sampled={ind_per_node_sampled}
+    """
+
+    # Write to file
+    with open(f'{output_dir}/GSpaceSettings_r_{r}.txt', "w") as f:
+        f.write(gspace_settings)
+
+    print(f"GSpaceSettings_r_{r}.txt generated with circular sampling in {output_dir}!")
