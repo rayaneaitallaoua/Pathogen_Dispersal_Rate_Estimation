61be1bc7827f97f17e695c775dc3a417a936ee5a
Rayane Ayoub AIT ALLAOUA <ayoubaitallaoua@gmail.com>
2025-06-19 09:55:30 +0200

    beast crash due to too many sequences? testing with less

--- dispersal_rate_per_radius/GSpace_simulations.py
+++ dispersal_rate_per_radius/GSpace_simulations.py
@@ -8,9 +8,10 @@ from Bio import SeqIO
 
 mcmc_chain_length = 10000000
 g_mut_rate = 1E-6
-radii = [10,20,30,40,50]
-rep_per_radius = 50
-nodes_sampled = 20
+#radii = [10,20,30,40,50]
+radii = [10]
+rep_per_radius = 1
+nodes_sampled = 6
 ind_per_node = 5
 latt_size_X = 100
 latt_size_Y = 100
