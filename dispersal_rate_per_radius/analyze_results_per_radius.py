7e6f2e0412bd1aa31ebf355ec21b006276d590eb
Rayane Ayoub AIT ALLAOUA <ayoubaitallaoua@gmail.com>
2025-07-03 14:49:13 +0200

    increased dpi for figs

--- dispersal_rate_per_radius/analyze_results_per_radius.py
+++ dispersal_rate_per_radius/analyze_results_per_radius.py
@@ -190,6 +190,6 @@ def extract_empirical_dispersal(full_output="empirical_dispersal_all.tsv",
         axs[i].grid(True)
 
     plt.tight_layout()
-    plt.savefig("empirical_dispersal_moments_by_radius.png")
+    plt.savefig("empirical_dispersal_moments_by_radius.png",dpi=300)
 
 extract_empirical_dispersal()
