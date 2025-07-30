d659e3b3852f1606b4f7f9b7a2ca8a1792b70181
Rayane Ayoub AIT ALLAOUA <ayoubaitallaoua@gmail.com>
2025-07-30 14:30:26 +0200

    Remove file from repository but keep it locally

--- diff_rate_estimation_comp_calcul_vs_beast_per_radius/checking_for_missing_fastas.sh
+++
@@ -1,8 +0,0 @@
-for dir in dist*_sim*; do
-  dx=$(echo "$dir" | sed -E 's/dist([0-9]+)_sim[0-9]+/\1/')
-  sim=$(echo "$dir" | sed -E 's/dist[0-9]+_sim([0-9]+)/\1/')
-  expected="sim_seqs_maxDist_${dx}_${dx}_Fasta.fa"
-  if [ ! -f "$dir/$expected" ]; then
-    echo "[MISSING] $expected not found in $dir (dist=$dx, sim=$sim)"
-  fi
-done
